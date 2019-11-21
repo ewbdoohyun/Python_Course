38cfb997d816f6671e81f6294a497605bc1219c0
ewbdoohyun <ewb.doohyun@gmail.com>
2019-11-21 12:47:36 +0000

    get job

--- main.py
+++ main.py
@@ -1,23 +1,6 @@
-import requests
-from bs4 import BeautifulSoup
+from indeed import extract_indeed_pages,extract_indeed_jobs
 
-indeed_result = requests.get("https://www.indeed.com/jobs?q=python&limit=50")
+last_indeed_page = extract_indeed_pages()
 
-indeed_soap = BeautifulSoup(indeed_result.text,'html.parser')
-#print(indeed_soap)
 
-# print(indeed_soap.title)
-
-pagination = indeed_soap.find("div", {"class":"pagination"})
-
-#print(pagingation)
-
-links = pagination.find_all('a')
-pages = []
-for link in links[:-1]:
-  pages.append(int(link.string))
-  # print(page.find("span"))
-
-max_pages = pages[-1]
-
-#print(spans)
+extract_indeed_jobs(last_indeed_page)
