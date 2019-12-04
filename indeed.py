bdd1a54f5307edb6ab890c8f9bb4e7462c2cbdee
ewbdoohyun <ewb.doohyun@gmail.com>
2019-12-04 11:59:10 +0000

    getting title, company, location, link

--- indeed.py
+++ indeed.py
@@ -4,27 +4,49 @@ from bs4 import BeautifulSoup
 LIMIT = 50
 URL = "https://www.indeed.com/jobs?q=python&limit={LIMIT}"
 
+
 def extract_indeed_pages():
-  result = requests.get(URL)
+    result = requests.get(URL)
 
-  soap = BeautifulSoup(result.text,'html.parser')
-  pagination = soap.find("div", {"class":"pagination"})
+    soap = BeautifulSoup(result.text, 'html.parser')
+    pagination = soap.find("div", {"class": "pagination"})
 
-  links = pagination.find_all('a')
-  pages = []
-  for link in links[:-1]:
-    pages.append(int(link.string))
+    links = pagination.find_all('a')
+    pages = []
+    for link in links[:-1]:
+        pages.append(int(link.string))
 
-  max_page = pages[-1]
-  return max_page
+    max_page = pages[-1]
+    return max_page
+
+
+def extract_job(html):
+    title = html.find("div", {"class": "title"}).find("a")["title"]
+    company = html.find("span", {"class": "company"})
+    company_anchor = company.find("a")
+    if company_anchor is not None:
+        company = str(company.find("a").string)
+    else:
+        company = str(company.string)
+    company = company.strip()
+    location = html.find("div", {"class": "recJobLoc"})["data-rc-loc"]
+    job_id = html["data-jk"]
+    return {
+        'title': title,
+        'company': company,
+        'loction': location,
+        "link" : f"https://www.indeed.com/viewjob?jk={job_id}"
+    }
+
 
 def extract_indeed_jobs(last_page):
-  jobs = []
-  for page in range(last_page):
-    result = requests.get(f"{URL}&start={page*LIMIT}")
-    soup = BeautifulSoup(result.text, "html.parser")
-    results = soup.find_all("div", {"class": "jobsearch-SerpJobCard"})
-    for result in results:
-      title = result.find("div",{"class":"title"}).find("a")["title"]
-      print(title)
-  return jobs
+    jobs = []
+    for page in range(last_page):
+      print(f"Scrapping page {page}")
+      result = requests.get(f"{URL}&start={page*LIMIT}")
+      soup = BeautifulSoup(result.text, "html.parser")
+      results = soup.find_all("div", {"class": "jobsearch-SerpJobCard"})
+      for result in results:
+          job = extract_job(result)
+          jobs.append(job)
+    return jobs
