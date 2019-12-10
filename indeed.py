35602611d6a56f46e12f7647a69af3a840fd3460
ewbdoohyun <ewb.doohyun@gmail.com>
2019-12-10 11:51:42 +0000

    add stackoverflow and export to csv file

--- indeed.py
+++ indeed.py
@@ -5,7 +5,7 @@ LIMIT = 50
 URL = "https://www.indeed.com/jobs?q=python&limit={LIMIT}"
 
 
-def extract_indeed_pages():
+def get_last_page():
     result = requests.get(URL)
 
     soap = BeautifulSoup(result.text, 'html.parser')
@@ -24,11 +24,14 @@ def extract_job(html):
     title = html.find("div", {"class": "title"}).find("a")["title"]
     company = html.find("span", {"class": "company"})
     company_anchor = company.find("a")
-    if company_anchor is not None:
-        company = str(company.find("a").string)
+    if company:
+      if company_anchor is not None:
+          company = str(company.find("a").string)
+      else:
+          company = str(company.string)
+      company = company.strip()
     else:
-        company = str(company.string)
-    company = company.strip()
+      company = None
     location = html.find("div", {"class": "recJobLoc"})["data-rc-loc"]
     job_id = html["data-jk"]
     return {
@@ -39,7 +42,7 @@ def extract_job(html):
     }
 
 
-def extract_indeed_jobs(last_page):
+def extract_jobs(last_page):
     jobs = []
     for page in range(last_page):
       print(f"Scrapping page {page}")
@@ -50,3 +53,8 @@ def extract_indeed_jobs(last_page):
           job = extract_job(result)
           jobs.append(job)
     return jobs
+
+def get_jobs():
+  last_page = get_last_page()
+  jobs = extract_jobs(last_page)
+  return jobs
--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-from indeed import extract_indeed_pages,extract_indeed_jobs
+from indeed import get_jobs as get_indeed_jobs
+from so import get_jobs as get_so_jobs
+from save import save_to_file
 
-last_indeed_page = extract_indeed_pages()
-
-
-extract_indeed_jobs(last_indeed_page)
+so_jobs = get_so_jobs()
+indeed_jobs = get_indeed_jobs()
+jobs = so_jobs + indeed_jobs
+save_to_file(jobs)
